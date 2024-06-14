a071cab6b5d86c6399c929b7058db23f02138b80
neojarvis <techadmin@iamneo.ai>
2024-06-14 06:44:23 +0000

    Auto-save-commit-14/06/2024--12:14:22

--- LINQ_2/dotnetapp/Models/Course.cs
+++ LINQ_2/dotnetapp/Models/Course.cs
@@ -4,10 +4,11 @@ namespace dotnetapp.Models
 {
 public class Course
 {
-    public int CourseId { get; set; } // Primary Key
-    public string CourseName { get; set; }
-    public string Description { get; set; }
+    public int Id { get; set; } // Primary Key
+    public string Name { get; set; }
+    public string Instructor { get; set; }
     public int Credits { get; set; }
     public ICollection<Student> Students { get; set; }
 }
 }
+
--- LINQ_2/dotnetapp/Models/Student.cs
+++ LINQ_2/dotnetapp/Models/Student.cs
@@ -1,16 +1,18 @@
+using System;
 using System.Collections.Generic;
 
 namespace dotnetapp.Models
 {
-public class Student
-{
-    public int StudentId { get; set; } // Primary Key
-    public string FirstName { get; set; }
-    public string LastName { get; set; }
-    public string Email { get; set; }
+    public class Student
+    {
+        public int Id { get; set; } // Primary Key
+        public string StudentNumber { get; set; }
+        public string Name { get; set; }
+        public DateTime EnrollmentDate { get; set; }
 
-    // Foreign key for the Course
-    public int? CourseId { get; set; }
-    public Course? Course { get; set; } // Navigation property
-}
+        // Foreign key for the Course
+        public int? CourseId { get; set; }
+        public Course? Course { get; set; } // Navigation property
+
+    }
 }
