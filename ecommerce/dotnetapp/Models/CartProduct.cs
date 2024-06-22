60ed4c77cc3daa598c9cd1830d28d52730ab9fe0
neojarvis <techadmin@iamneo.ai>
2024-06-22 07:40:28 +0000

    Auto-save-commit-22/06/2024--13:10:24

--- ecommerce/dotnetapp/Models/CartProduct.cs
+++ ecommerce/dotnetapp/Models/CartProduct.cs
@@ -5,9 +5,7 @@ namespace dotnetapp.Models
 {
 public class CartProduct
     {
-        public int CartId { get; set; }
-        public int ProductId { get; set; }
-        public int Quantity { get; set; }
         public Product Product { get; set; }
+        public int Quantity { get; set; }
     }
 }
